c61805dd2f4b6dde1c71cf7eb7b20e74891d716c
MK404xL <shinydark.sky@gmail.com>
2020-07-21 18:25:43 +0700

    Show list todo demo

--- src/Components/TodoLists.js
+++ src/Components/TodoLists.js
@@ -4,14 +4,18 @@ import Todo from './Todo'
 class TodoLists extends React.Component{
     constructor(props){
         super(props)
-        this.state = {todo:['Tin Học Căn Bản','Lập trình Web']}
+        this.state = {todo:['Tin Học Căn Bản','Lập trình Web','Cơ sỡ dữ liệu','Lý thuyết đồ thị']}
     }
-    renderTodo=()=>{
-        return <Todo />
+    renderTodo(){
+        var Todos = this.state.todo
+
+        return Todos.map(function(value,index){
+            return <Todo todo={value} key={index}/>
+        })
     }
     render(){
         return(
-            <Todo/>
+            this.renderTodo()    
         )
     }
 }
